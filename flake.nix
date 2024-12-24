24656254df406aa43a9b1b7e6b142c86f4452f16
Jonathan Anbary <jonathananbary@gmail.com>
2024-12-24 17:14:54 +0200

    moved to using flakes so that I can make use of the master branch of zig

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Patch binary files with ease";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    zig-overlay.url = "github:mitchellh/zig-overlay";
+    zls-overlay.url = "github:zigtools/zls";
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    ...
+  }:
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    zig = inputs.zig-overlay.packages.x86_64-linux.master;
+    # zig = inputs.zig-overlay.packages.x86_64-linux.default;
+    zls = inputs.zls-overlay.packages.x86_64-linux.zls.overrideAttrs (old: {
+            nativeBuildInputs = [ zig ];
+          });
+  in
+  {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = [
+        zls
+        zig
+      ];
+    };
+  };
+}
