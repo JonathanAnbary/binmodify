e6a2589674debd101c8023e164c0e2c7f667c445
Jonathan Anbary <jonathananbary@gmail.com>
2025-02-03 12:46:09 +0200

    wine doesnt really work well for testing for me, but I think everything is working properly

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   description = "Patch binary files with ease";
   inputs = {
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     zig-overlay.url = "github:mitchellh/zig-overlay";
     zls-overlay.url = "github:zigtools/zls";
@@ -10,10 +11,12 @@
   outputs = inputs @ {
     self,
     nixpkgs,
+    nixpkgs-stable,
     ...
   }:
   let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    pkgs-stable = nixpkgs-stable.legacyPackages.x86_64-linux;
     zig = inputs.zig-overlay.packages.x86_64-linux.master;
     # zig = inputs.zig-overlay.packages.x86_64-linux."0.13.0";
     zls = inputs.zls-overlay.packages.x86_64-linux.zls.overrideAttrs (old: {
@@ -25,9 +28,8 @@
       packages = [
         zls
         zig
-        pkgs.wine64
-        pkgs.wineWowPackages.stable
-        pkgs.winetricks
+        pkgs-stable.wine64
+        pkgs-stable.wineWowPackages.stable
         (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
         # select Python packages here
         ipython
